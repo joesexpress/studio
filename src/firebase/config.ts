e4c023efcda7a6a0a0bd6ec9550ae43726cd2a10
Jake Betts <Jake@joespizzaexpress.net>
2026-01-13 02:45:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-923237216-30b76",
+  "appId": "1:754332991690:web:b8ae6be24ebc0a4a7f3433",
+  "apiKey": "AIzaSyBgE7Kd7EbzyZ34PvXnpi4bllvOrvLWKAk",
+  "authDomain": "studio-923237216-30b76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "754332991690"
+};
