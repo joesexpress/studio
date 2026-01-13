5ed662f6e0d7e872f518eaf5d2ae782c58b6a0fe
Jake Betts <Jake@joespizzaexpress.net>
2026-01-13 03:24:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,11 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  experimental: {
+    // This is required to allow the Next.js dev server to accept requests from
+    // the Cloud Workstation and Firebase Studio.
+    allowedNextBundlerReactRoots: ['/home/user/studio'],
+  },
 };
 
 export default nextConfig;
