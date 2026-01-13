30900f38ebe8af5a801143b821583f38604eac6e
Jake Betts <Jake@joespizzaexpress.net>
2026-01-13 03:25:50 +0000

    wont load

--- next.config.ts
+++ next.config.ts
@@ -35,11 +35,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  experimental: {
-    // This is required to allow the Next.js dev server to accept requests from
-    // the Cloud Workstation and Firebase Studio.
-    allowedNextBundlerReactRoots: ['/home/user/studio'],
-  },
 };
 
 export default nextConfig;
