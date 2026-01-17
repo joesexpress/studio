69de7fcfc7d15ffa077bab7278d27a491de49104
Jake Betts <Jake@joespizzaexpress.net>
2026-01-17 21:14:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
